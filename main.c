8fc9ae2adddb49ebd56e50af4400c6c7cda273de
anik904-lang <hossainanik904@gmail.com>
2025-10-06 11:02:31 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int marks[]={10,20,30,40,50};
+    int n=5;
+
+    for(int i=0;i<n;i++){
+
+    printf("%d\n",marks[i]);
+    }
+
+
+    printf("\nmarks of cdna ");
+    return 0;
+}
