7a6c7197f9e06be1a53bf539e028fb26659f9c51
anik904-lang <hossainanik904@gmail.com>
2025-10-14 22:23:26 +0600

    Create simple calculator.cpp

---
+++ simple calculator.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+    cin>>a>>b;
+    int add=a+b,red=a-b,mul=a*b,div=a/b,mod=a%b;
+    cout<<add<<" "<<red<<" "<<mul<<" "<<div<<" "<<mod<<endl;
+     return 0;
+}
