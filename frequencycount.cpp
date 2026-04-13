138359036adac7df78637f562f23c9e92133eeca
Anik Hossain <hossainanik904@gmail.com>
2026-04-13 23:23:17 +0600

    Add files via upload

---
+++ frequencycount.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int sumArray(int* arr, int n) {
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    cout << sumArray(arr, n) << endl;
+
+    return 0;
+}
